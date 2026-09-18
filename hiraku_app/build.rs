44abac8ff06a16082a865c120d395d904f409f14
LaoLittle <laolittle@icloud.com>
2026-09-18 02:16:42 -0500

    fix build ci

--- hiraku_app/build.rs
+++ hiraku_app/build.rs
@@ -1,8 +1,10 @@
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("missing target OS");
-    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").expect("missing target architecture");
 
-    if target_os == "android" && target_arch == "aarch64" {
-        println!("cargo::rustc-link-arg-cdylib=-Wl,-Bsymbolic");
+    match &*target_os {
+        "android" | "linux" => {
+            println!("cargo::rustc-link-arg-cdylib=-Wl,-Bsymbolic");
+        }
+        _ => {}
     }
 }
